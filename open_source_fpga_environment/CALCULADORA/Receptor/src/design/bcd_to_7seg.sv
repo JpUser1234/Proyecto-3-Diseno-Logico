cb68c08277c8f5e916f141f2139d8061895d11ef
GCHAVES777 <g.chaves.1@estudiantec.cr>
2026-05-04 22:50:42 -0600

    CERO EN MITAD

--- open_source_fpga_environment/CALCULADORA/Receptor/src/design/bcd_to_7seg.sv
+++ open_source_fpga_environment/CALCULADORA/Receptor/src/design/bcd_to_7seg.sv
@@ -1,9 +1,9 @@
 module bcd_to_7seg (
-    input  wire [3:0] digit,
-    output reg  [6:0] seg
+    input [3:0] digit,
+    output reg [6:0] seg
 );
 
-always_comb begin
+always @(*) begin
     case (digit)
         4'd0: seg = 7'b1111110;
         4'd1: seg = 7'b0110000;
--- open_source_fpga_environment/CALCULADORA/Receptor/src/design/display_mux.sv
+++ open_source_fpga_environment/CALCULADORA/Receptor/src/design/display_mux.sv
@@ -1,15 +1,16 @@
 module display_mux (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire [3:0]  digit0,
-    input  wire [3:0]  digit1,
-    input  wire [3:0]  digit2,
-    input  wire [3:0]  digit3,
-    output reg  [3:0]  anode,
-    output reg  [3:0]  digit_out
+    input  wire       clk,
+    input  wire       rst,
+    input  wire [3:0] digit0,
+    input  wire [3:0] digit1,
+    input  wire [3:0] digit2,
+    input  wire [3:0] digit3,
+    output reg  [3:0] anode,
+    output reg  [3:0] digit_out
 );
 
 localparam MAX_COUNT = 27_000;
+
 reg [14:0] counter;
 reg [1:0]  sel;
 
@@ -17,24 +18,25 @@ always_ff @(posedge clk) begin
     if (!rst) begin
         counter   <= 0;
         sel       <= 0;
-        anode     <= 4'b0001;
+        anode     <= 4'b0000;
         digit_out <= 4'd0;
     end else begin
-        if (counter == MAX_COUNT - 1) begin
+
+        // apagar TODO antes de cambiar
+        anode <= 4'b0000;
+
+        if (counter == MAX_COUNT-1) begin
             counter <= 0;
-            if (sel == 2'd3)
-                sel <= 0;
-            else
-                sel <= sel + 1;
+            sel <= sel + 1;
         end else begin
             counter <= counter + 1;
         end
 
         case (sel)
-            2'd0: begin anode <= 4'b0001; digit_out <= digit0; end
-            2'd1: begin anode <= 4'b0010; digit_out <= digit1; end
-            2'd2: begin anode <= 4'b0100; digit_out <= digit2; end
-            2'd3: begin anode <= 4'b1000; digit_out <= digit3; end
+            2'd0: begin digit_out <= digit0; anode <= 4'b0001; end
+            2'd1: begin digit_out <= digit1; anode <= 4'b0010; end
+            2'd2: begin digit_out <= digit2; anode <= 4'b0100; end
+            2'd3: begin digit_out <= digit3; anode <= 4'b1000; end
         endcase
     end
 end
